7c6b0256976f1e7a7e3fe25909e8d41182b62aac
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-03-24 00:15:20 +0530

    Added qr code generation

--- pages/generator.tsx
+++ pages/generator.tsx
@@ -1,4 +1,37 @@
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
+import styles from "../styles/Home.module.css";
+
 const generator = () => {
+  const [qrCodeValue, setQrCodeValue] = useState("");
+  const [formData, setFormData] = useState({
+    payeeName: "",
+    upiId: "",
+    transactionAmount: 0,
+    description: "",
+  });
+
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("count") == null) {
+      localStorage.setItem("count", String(0));
+    }
+  }
+  const generateQR = (e) => {
+    e.preventDefault();
+    
+    if(formData.payeeName == "" || formData.upiId == "" || formData.transactionAmount == 0 || formData.description == ""){
+        alert("Please fill all the fields");
+        return;
+    }
+    const qrValue = `upi://pay?pa=${formData.upiId}&pn=${formData.payeeName}&am=${formData.transactionAmount}&tn=${formData.description}&cu=INR`;
+    setQrCodeValue(qrValue);
+    if (typeof window !== "undefined") {
+      console.log("window is undefined");
+      let count = localStorage.getItem("count");
+      localStorage.setItem("count", String(Number(count) + 1));
+    }
+  };
+
   return (
     <div className="flex flex-col items-center w-full h-screen overflow-hidden bg-slate-100">
       <div className="flex flex-col items-center m-auto">
@@ -16,6 +49,10 @@ const generator = () => {
             <div className="space-y-2">
               <label htmlFor="">Payee/Merchant Name</label>
               <input
+                value={formData.payeeName}
+                onChange={(e) =>
+                  setFormData({ ...formData, payeeName: e.target.value })
+                }
                 type="text"
                 className="w-full p-2 border border-slate-300 rounded-lg focus:outline-none focus:ring focus:border-slate-400"
                 placeholder="Payee Name"
@@ -24,6 +61,10 @@ const generator = () => {
             <div className="space-y-2">
               <label htmlFor="">UPI ID</label>
               <input
+                value={formData.upiId}
+                onChange={(e) =>
+                  setFormData({ ...formData, upiId: e.target.value })
+                }
                 type="text"
                 className="w-full p-2 border border-slate-300 rounded-lg focus:outline-none focus:ring focus:border-slate-400"
                 placeholder="UPI ID"
@@ -33,7 +74,14 @@ const generator = () => {
               {" "}
               <label htmlFor="">Transaction Amount</label>
               <input
-                type="text"
+                value={formData.transactionAmount}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    transactionAmount: Number(e.target.value),
+                  })
+                }
+                type="number"
                 className="w-full p-2 border border-slate-300 rounded-lg focus:outline-none focus:ring focus:border-slate-400"
                 placeholder="Transaction Amount"
               />
@@ -42,14 +90,39 @@ const generator = () => {
               {" "}
               <label htmlFor="">Description (Notes)</label>
               <input
+                value={formData.description}
+                onChange={(e) =>
+                  setFormData({ ...formData, description: e.target.value })
+                }
                 type="text"
                 className="w-full p-2 border border-slate-300 rounded-lg focus:outline-none focus:ring focus:border-slate-400"
                 placeholder="Description (Notes)"
               />
             </div>
-            {/* make a download button */}
+            <button
+              onClick={generateQR}
+              className="w-full p-2 bg-sky-600 text-white rounded-lg"
+            >
+              Generate QR code
+            </button>
           </form>
-          <div>asd</div>
+          <div>
+            <div>
+              <div>Generate QR</div>
+
+              {qrCodeValue != "" && (
+                <QRCode
+                  value={qrCodeValue}
+                  className={styles.containerColumn}
+                />
+              )}
+              <input
+                onChange={(e) => {
+                  setQrCodeValue(e.target.value);
+                }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
