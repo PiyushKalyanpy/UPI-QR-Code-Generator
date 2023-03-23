d417a574e50c40758beb38f33fbaa79710c56ff3
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-03-24 02:44:09 +0530

    Bugs Fixed  , UI imporoved, responsive UI

--- pages/generator.tsx
+++ pages/generator.tsx
@@ -38,25 +38,32 @@ const Generator = () => {
   };
 
   return (
-    <div className="flex flex-col items-center w-full h-screen overflow-hidden bg-slate-100">
-      <div className="flex flex-col items-center m-auto">
-        <div className="flex flex-col items-center ">
-          <h1 className="font-bold text-4xl text-slate-800 ">
+    <div className="flex flex-col items-center w-full md:h-screen max:h-fit   overflow-hidden bg-slate-100">
+      <div className="flex flex-col items-center m-auto ">
+        <div className="flex flex-col space-y-4 items-center sm:pt-10">
+          <h1 className="font-bold font-urbanist  text-4xl pt-10 lg:pt-0 text-slate-800 ">
             Generate UPI QR code{" "}
           </h1>
-          <h2 className=" text-xl text-slate-500">
-            Simplify your payment process
+          <h2 className=" text-xl font-inter text-slate-500 text-center">
+            Simplify your payment process with
           </h2>
+          <h1 className="text-sky-500 font-extrabold text-3xl m-2 bg-gradient-to-r from-blue-600 via-sky-400 to-pink-400 inline-block text-transparent bg-clip-text font-urbanist">
+            {" "}
+            ShufflePay
+          </h1>
         </div>
-        <div className="grid grid-cols-2 w-10/12 bg-white p-8 m-10 rounded-3xl h-fit ">
+        <div className="grid lg:grid-cols-2 gap-10  w-10/12 lg:w-1/2 bg-white p-8 m-10 rounded-3xl h-fit ">
           {/* div for User Inputs */}
+
           <form
             autoComplete="on"
             action=""
-            className="m-auto w-10/12 space-y-4 autofill:bg-white "
+            className="m-auto  md:w-10/12 space-y-4 autofill:bg-white "
           >
             <div className="space-y-2">
-              <label htmlFor="payeeName">Payee/Merchant Name</label>
+              <label htmlFor="payeeName" className="text-slate-500">
+                Payee/Merchant Name
+              </label>
               <input
                 name="payeeName"
                 value={formData.payeeName}
@@ -69,7 +76,9 @@ const Generator = () => {
               />
             </div>
             <div className="space-y-2">
-              <label htmlFor="upiId">UPI ID</label>
+              <label htmlFor="upiId" className="text-slate-500">
+                UPI ID
+              </label>
               <input
                 name="upiId"
                 value={formData.upiId}
@@ -83,7 +92,9 @@ const Generator = () => {
             </div>
             <div className="space-y-2">
               {" "}
-              <label htmlFor="transactionAmount">Transaction Amount</label>
+              <label htmlFor="transactionAmount" className="text-slate-500">
+                Transaction Amount
+              </label>
               <input
                 min={0}
                 max={5000}
@@ -101,7 +112,9 @@ const Generator = () => {
             </div>
             <div className="space-y-2">
               {" "}
-              <label htmlFor="description">Description (Notes)</label>
+              <label htmlFor="description" className="text-slate-500">
+                Description (Notes)
+              </label>
               <input
                 name="description"
                 value={formData.description}
@@ -120,62 +133,56 @@ const Generator = () => {
               Generate QR code
             </button>
           </form>
-          <div>
-            <div className="relative flex-col h-full ">
-              
-              <div className="relative my-4   mx-auto rounded-2xl overflow-hidden  h-1/2 items-center">
-                <Image
-                  src="./brandhive.svg"
-                  width="40"
-                  alt="gpay"
-                  height="30"
-                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
-                />
-                {qrCodeValue != "" && (
-                  <QRCode
-                    value={qrCodeValue}
-                    className="w-full h-full text-red-300"
-                  />
-                )}
-              </div>
 
-              <div className="absolute bottom-8">
-                <div className="flex flex-col space-y-10 ">
-                  <div className="grid grid-cols-2 gap-4 items-center justify-items-center ">
-                    <Image
-                      src="./up01.svg"
-                      width="130"
-                      alt="gpay"
-                      height="30"
-                    />
-                    <Image src="./up02.svg" width="90" alt="gpay" height="30" />
-                  </div>
-                  <div className="grid grid-cols-4 gap-4 items-center justify-items-center ">
-                    <Image
-                      src="./bottom02.svg"
-                      width="50"
-                      alt="gpay"
-                      height="30"
-                    />
-                    <Image
-                      src="./bottom01.svg"
-                      width="50"
-                      alt="gpay"
-                      height="30"
-                    />
-                    <Image
-                      src="./bottom03.svg"
-                      width="80"
-                      alt="gpay"
-                      height="30"
-                    />
-                    <Image
-                      src="./bottom04.svg"
-                      width="140"
-                      alt="gpay"
-                      height="60"
-                    />
-                  </div>
+          {/* div for QR Code */}
+
+          <div className="flex flex-col  gap-4 h-full">
+            <div className="relative my-4  w-3/4 md:w-1/2 md:mt-10 m-auto md:h-64 mx-auto rounded-3xl overflow-hidden  items-center">
+              <Image
+                src="./brandhive.svg"
+                width="40"
+                alt="ShufflePay"
+                height="30"
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
+              />
+              {qrCodeValue != "" && (
+                <QRCode
+                  value={qrCodeValue}
+                  className="w-full h-full rounded-3xl overflow-hidden "
+                />
+              )}
+            </div>
+            <div className="flex my-4  h-1/2 mx-auto  overflow-hidden  items-center">
+              <div className="flex flex-col space-y-10  mt-auto ">
+                <div className="grid grid-cols-2 gap-4 items-center justify-items-center ">
+                  <Image src="./up01.svg" width="130" alt="gpay" height="30" />
+                  <Image src="./up02.svg" width="90" alt="gpay" height="30" />
+                </div>
+                <div className="grid grid-cols-4 gap-4 items-center justify-items-center ">
+                  <Image
+                    src="./bottom02.svg"
+                    width="45"
+                    alt="gpay"
+                    height="30"
+                  />
+                  <Image
+                    src="./bottom01.svg"
+                    width="45"
+                    alt="gpay"
+                    height="30"
+                  />
+                  <Image
+                    src="./bottom03.svg"
+                    width="100"
+                    alt="gpay"
+                    height="30"
+                  />
+                  <Image
+                    src="./bottom04.svg"
+                    width="190"
+                    alt="gpay"
+                    height="60"
+                  />
                 </div>
               </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,19 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   important: true,
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        urbanist: ["Urbanist", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
