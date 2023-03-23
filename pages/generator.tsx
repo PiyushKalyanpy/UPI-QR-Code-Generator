00371cd6dbeb90358472436cc77727e1e44427a5
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-03-23 21:52:29 +0530

    Project Initialized

---
+++ pages/generator.tsx
@@ -0,0 +1,11 @@
+
+const generator = () => {
+    return (
+        <div>
+            <h1>Generate UPI QR code </h1>
+            <h2>Simplify your payment process</h2>
+        </div>
+    );
+}
+
+export default generator;
